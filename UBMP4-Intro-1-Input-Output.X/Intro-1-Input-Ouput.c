aab67c4417b5ff3337217bca05662484ec2eb585
John Rampelt <johnrampelt@gmail.com>
2023-12-13 13:16:48 -0500

    Rebuilt project with latest compiler and toolchain

--- UBMP4-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
+++ UBMP4-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
@@ -1,10 +1,11 @@
 /*==============================================================================
  Project: Intro-1-Input-Output          Activity: mirobo.tech/intro-1
- Date:    August 14, 2023
+ Date:    December 12, 2023
  
  This introductory input and output programming activity for the mirobo.tech
- UBMP4 demonstrates pushbutton input, LED (bit) output, port (byte) output,
- MPLAB's built-in time delay functions, and simple 'if' condition structures.
+ CHRP4 and UBMP4 demonstrates pushbutton input, LED (bit) output, port (byte)
+ output, the use of MPLAB's built-in time delay functions, and simple 'if'
+ condition structures.
  
  Additional program analysis and programming activities demonstrate the logical
  AND and OR conditional operators, the use of time delay functions to create
@@ -20,16 +21,16 @@
 // TODO Set linker ROM ranges to 'default,-0-7FF' under "Memory model" pull-down.
 // TODO Set linker code offset to '800' under "Additional options" pull-down.
 
-// The main function is required in every C program. The microcontroller begins
-// executing the program starting at the first line in the main function.
+// The main function is a required part of every C program. The microcontroller
+// begins executing the program starting at the first line in the main function.
 
 int main(void)
 {
-    // These configuration functions run once during program start-up.
+    // The configuration functions run once during program start-up.
     OSC_config();               // Configure internal oscillator for 48 MHz
     UBMP4_config();             // Configure I/O for on-board UBMP4 devices
     
-    // The code in this while loop repeats forever.
+    // The contents of the while loop repeat continuously.
     while(1)
 	{
         // If SW2 is pressed, make a flashy light pattern
@@ -53,7 +54,7 @@ int main(void)
             __delay_ms(100);
         }
 
-        // Add your Program Analysis and Programming Activities code here:
+        // Add your Program Analysis Activities and Programming Activities code here:
 
         // Reset the microcontroller and start the bootloader if SW1 is pressed.
         if(SW1 == 0)
@@ -66,22 +67,23 @@ int main(void)
 /* Learn More -- Program Analysis Activities
  * 
  * 1. Analyzing a program to understand its operation is an important skill to
- *    develop. Let's figure out how this program works! Start by testing the
- *    program and observing what happens when it runs in your circuit board.
- *    Quickly press and release SW2 and watch what the light pattern does. How
- *    many lights flash? How many times do they flash? Next, press and hold SW2
- *    and observe the light pattern. Does the light pattern stop immediately
- *    when you let go of SW2?
+ *    develop. Let's analyze this program to understand it! Start by running the
+ *    program in your circuit board. Press and immediately release SW2 and watch
+ *    what the LEDs do. How many LEDs flash? How many times to they flash? Next,
+ *    press and hold SW2 and observe the LEDs and flashing pattern. Does the
+ *    pattern stop immediately when you let go of SW2?
  * 
  *    Now, examine the program and try to match your observations of the light
- *    pattern to the program code. How many of the lights are controlled by the 
- *    pattern inside the curly braces following the SW2 'if' statement? If SW2
- *    is pressed should the entire light pattern continue until it is finished,
- *    or should it stop as soon as SW2 is released? Can you explain why that is?
+ *    pattern to the program code. How many LED control and delay statements
+ *    are grouped together inside the curly braces that follow the SW2 'if'
+ *    statement? Did all of these actions happen when you pressed and let go of
+ *    SW2? Can you explain why the microcontroller will always complete all of
+ *    the actions inside the braces, and not skip any of the steps each time
+ *    pushbutton SW2 is pressed?
  * 
- * 2. Explain the difference between the statements: LED2 = 0; and LED2 = 1;
- *    How does making LED2 become 0 or 1 in the code actually turn LED2 on or
- *    off on the circuit board?
+ * 2. Explain the difference between the statements: LED2 = 1; and LED2 = 0;
+ *    How does setting LED2 as 1 or 0 in the program code actually turn LED2 on
+ *    or off on the circuit board?
  * 
  * 3. Understanding how a circuit works is another important skill to develop
  *    in interfacing (controlling hardware with software). What voltage do you
@@ -94,12 +96,13 @@ int main(void)
  *    of the LEDs as the program outputs a 0, and again when it outputs a 1.
  *    The voltage will change quickly so you may need to use a 'peak hold' or
  *    'max' feature if your multimeter has one, or slow down the light pattern
- *    in your program by increasing the time delays. Next, refer to the 
- *    schematic to find one of the microcontroller pins that is connected to
- *    one of the LEDs, and measure between that microcontroller pin and the
- *    negative pin of battery connector BT1 as you run the program again.
- *    Compare your measurements. Which voltage matched your prediction? Can you
- *    explain why?
+ *    in your program by increasing the time delays.
+ * 
+ *    Next, refer to the schematic and find one of the microcontroller pins that
+ *    connects to one of the LEDs, and measure between the microcontroller pin
+ *    and the negative pin of battery connector BT1 on the circuit board as you
+ *    run the program and press SW2. Compare your measurements. Did one of these
+ *    voltages match your prediction? Which one? Can you explain why?
  * 
  * 4. The statement 'if(SW2 == 0)' contains two equal signs, while the statement
  *    'LED2 = 1;' contains a single equal sign. What operation is performed by a
@@ -112,13 +115,14 @@ int main(void)
  *    microcontroller's switch inputs will be low when the switch is pressed?
  *  
  * 6. The following code structure includes instructions that write to all of 
- *    the bits in the PORTC output latches (called LATC) directly. PORTC (LATC)
- *    connects to CHRP4's on-board LEDs. Let's try some new code to control
- *    the LEDs in a different way than the original SW2 output structure does.
+ *    the bits in the PORTC output latches (called LATC) directly. The four
+ *    LEDs controlled by this program are all connected to PORTC's LATC outputs.
+ *    Let's try another to control all of the LEDs by writing to LATC instead
+ *    of using individual LED commands.
  * 
- *    Copy and paste the following block of code underneath the existing SW2
- *    'if' structure in your program (paste it where shown by the code comment
- *     located at line 56 of the original program above).
+ *    Copy the block of code (below) and pasted it after the closing SW2 'if'
+ *    structure brace in your program -- the code comment located on line 56
+ *    of the program, above, shows where to paste this code.
 
         if(SW3 == 0)
         {
@@ -128,13 +132,14 @@ int main(void)
             __delay_ms(100);
         }
 
- *    What happens when pushbutton SW3 is pressed? Can you think of at least one
- *    advantage and one disadvantage of controlling the LEDs using 'LATC' write
- *    statements rather than by individual 'LEDn = x;' statements?
+ *    Re-build the program and download it into your circuit. What happens when
+ *    pushbutton SW3 is pressed? Can you think of at least one advantage and one
+ *    disadvantage of controlling all of the LEDs using 'LATC' statements rather
+ *    than by using individual 'LEDn = x;' statements?
  * 
  * 7. Next, let's compare the operation of 'if' and 'while' structures to
  *    simulate the operation of momentary buttons. Add the following code below
- *    the SW3 'if' structure you added in analysis activity 6, above:
+ *    the SW3 'if' structure that you added in step 6, above:
 
         // Momentary button using an if structure
         if(SW4 == 0)
@@ -154,8 +159,8 @@ int main(void)
         LED5 = 0;
 
  *    First, try pressing and releasing SW4 and SW5 one at a time. They should
- *    act the same way, turning their respective LEDs on when each switch is
- *    pressed.
+ *    act the same way, turning their respective LED on as each switch is
+ *    pressed, and off as each switch is released.
  * 
  *    Next, press and hold SW4 while pressing and releasing SW5. Does SW5 work
  *    as expected?
@@ -164,11 +169,11 @@ int main(void)
  *    as expected?
  * 
  *    Can you explain the difference in operation between the 'if' and 'while'
- *    structures making up the momentary button code.
+ *    structures in the two different examples of momentary button code?
  * 
  * 8. Let's explore logical conditions using 'if' statements. Replace the code
  *    added in step 7, above, with this 'nested if' code to make a logical AND
- *    condition which will light LED D4 only if both SW4 and SW5 are pressed:
+ *    condition that will light LED D4 only if both SW4 and SW5 are pressed:
 
         // Nested if 'AND' code
         if(SW4 == 0)
@@ -191,8 +196,8 @@ int main(void)
  *    conditions matter? (e.g. swap the conditional checks for SW4 and SW5)
  * 
  * 9. Next, replace the code from activity 8, above, with the following code
- *    which implements the logical AND conditional operator composed of two
- *    ampersands '&&':
+ *    which implements the logical AND conditional operator -- it's composed of
+ *    two ampersands '&&' and becomes part of the 'if' statement:
  
         // Conditional 'AND' code
         if(SW4 == 0 && SW5 == 0)
@@ -209,7 +214,7 @@ int main(void)
  *    nested if structures?
  * 
  * 10. Replace the double ampersand '&&' with double vertical bars '||' to make
- *    a logical OR conditional operator. Your code should look like this:
+ *    a logical OR conditional operator. Your code should now look like this:
   
         // Conditional 'OR' code
         if(SW4 == 0 || SW5 == 0)
@@ -238,7 +243,7 @@ int main(void)
  * 
  *    You won't be able to see microsecond length LED flashes with your eyes,
  *    but you can measure them using an oscilloscope, or hear them if they are
- *    used to turn a piezo beeper on and off. Try the following code in your
+ *    used to turn the piezo beeper on and off. Try the following code in your
  *    program:
  
         // Make a tone while SW5 is held
@@ -250,9 +255,9 @@ int main(void)
             __delay_us(567);
         }
 
- *    Try changing the delay values in both of the __delay_us(); functions.
- *    Does the pitch of the tone increase or decrease if the delay values are
- *    made smaller? Does the opposite happen if the delays are made larger?
+ *    Try changing the values used in both of the __delay_us(); functions. Does
+ *    the pitch of the tone increase or decrease if the delay values are made
+ *    smaller? Does the opposite happen if the delays are made larger?
  * 
  * 3. This code demonstrates a more compact way of 'toggling' the beeper output
  *    using a logical NOT operator '!'. Replace the code above, with this code:
@@ -265,17 +270,19 @@ int main(void)
         }
 
  *    One difference between this code and the code in activity 2, above, is the
- *    state of the BEEPER pin when SW5 is released. Will you know what state the
- *    BEEPER output will be in after this code runs? While one advantage of this
- *    method is less program code, can you think of one or more disadvantages of
- *    not knowing the output state of a pin when using code like this?
+ *    state of the BEEPER pin when SW5 is released. Because we don't know how
+ *    many times this code will run, we also won't know the state of the BEEPER
+ *    pin after the code runs. While one advantage of this method is using less
+ *    program code, can you think of one or more disadvantages caused by not
+ *    knowing the output state of a pin when using code like this?
  * 
  * 4. Using modified versions of the original SW2 'if' structure, create a
  *    program that makes a unique LED flashing pattern for each pushbutton.
  * 
- *    Test each of your flashing patterns. Describe what happens when more than
- *    one button is held. Do all of the patterns try to flash the LEDs at the
- *    same time, or sequentially? Explain why this is so.
+ *    Test each of your flashing patterns to verify that it works as expected.
+ *    Describe what happens when more than one button is held. Do all of the
+ *    patterns flash the LEDs at the same time, or in sequence? What sequence
+ *    do they follow? Explain why this happens.
  * 
  * 5. Create a program that makes a different tone for each pushbutton.
  * 
@@ -283,19 +290,19 @@ int main(void)
  *    more buttons at the same time. Describe what you think the tone waveforms
  *    will look like when two or more buttons are held. (You can verify your
  *    prediction if you have access to an oscilloscope and connect it across
- *    the piezo beeper's output resistor.)
+ *    the piezo beeper's output resistor on the circuit board.)
  * 
  * 6. Use individual 'if' structures to simulate 'Start' and 'Stop' buttons for
  *    an industrial machine. Use SW3 to turn LED D3 on when it is pressed, and
- *    make LED D3 stay on even after SW3 is released. Use another if structure
- *    for SW4 to turn LED D3 off when it is pressed. Test your program to make
- *    sure that it works the way you expect it to.
+ *    keep LED D3 on even after SW3 is released. Use another if structure for
+ *    SW4 to turn LED D3 off when it is pressed. Test your program to make sure
+ *    that it works the way you expect it to.
  * 
- * 7. Running your program from activity 6, above, describe what happens when
- *    both SW3 and SW4 are held. Does LED D3 stay on? If so, how does its
- *    brightness compare when both button are held to when only SW3 is pressed?
- *    If the brightness is different, can you explain what part of the code is
- *    causing it to change, and why it changes?
+ * 7. Running your program from step 6, above, describe what happens when both
+ *    pushbuttons SW3 and SW4 are held. Does LED D3 stay on? If so, it its
+ *    brightness the same when both buttons are held as when just SW3 is
+ *    pressed? If the brightness is different, can you explain what part of the
+ *    code might be causing it to change, and why it changes?
  * 
  * 8. As you might imagine, an industrial machine that is able to turn on even
  *    while its 'Stop' button is pressed represents a significant safety hazard.
@@ -305,18 +312,18 @@ int main(void)
  *    being pressed at the same time.
  * 
  * 9. LED D1 is normally used to indicate that a program is running, but it can
- *    be controlled by your program as well. If you examine the CHRP4 schematic,
- *    you will see that LED D1's cathode (or negative) pin is connected to the
- *    microcontroller instead of having its anode (positive) pin connected to
- *    the microcontroller as the other LEDs do. This means that the LED D1
- *    output must be made equal to 0 to turn D1 on.
+ *    be controlled by your program as well. If you examine your circuit's
+ *    schematic, you will see that LED D1's cathode (or negative) pin is
+ *    connected to the microcontroller instead of its anode (positive) as with
+ *    the other LEDs. This means that the LED D1 output must be made a low
+ *    voltage to turn D1 on.
  * 
  *    Try it! Make a program that controls or flashes LED D1 as part of a light
  *    pattern. (Be careful that you don't change the SW1 reset code when making
- *    your light pattern, or you may not be able to use switch SW1 to reset
- *    the board and enter programming mode using the bootloader. If this
- *    happens, unplug the USB cable and press and hold SW1 while re-connecting
- *    the USB cable. LED D1 will remain off until you release SW1, and then the
- *    bootloader will start. Then, be sure to re-enable the SW1 reset code that
- *    your code edits accidentally disabled.)
+ *    your light pattern, or you may not be able to use pushbutton SW1 to reset
+ *    the board and start the bootloader. If this ever happens, simply unplug
+ *    the USB cable and press and hold SW1 while re-connecting the USB cable.
+ *    LED D1 will remain off until you release SW1, and the bootloader will be
+ *    forced to start after SW1 is released. Then, be sure to re-enable the SW1
+ *    reset code that your code edits accidentally disabled.)
  */
